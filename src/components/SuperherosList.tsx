6ea3302fc999ad863192f6e7e7b1d1ffc5e3e394
arturmai <arturmai@radware.com>
2023-08-31 11:07:31 +0300

    add message

--- src/components/SuperherosList.tsx
+++ src/components/SuperherosList.tsx
@@ -2,12 +2,14 @@ import Superhero from "./Superhero";
 import { ISuperherosList } from "../types";
 
 const SuperherosList: React.FC<ISuperherosList> = ({ superheros }) => {
-  return (
-    superheros &&
-    superheros.length > 0 &&
+  return superheros && superheros.length > 0 ? (
     superheros.map((superhero) => (
       <Superhero key={superhero.id} {...superhero} />
     ))
+  ) : (
+    <h3 className="text-2xl font-bold text-gray-900 dark:text-white">
+      Search for Superhero to see results 🔍
+    </h3>
   );
 };
 
